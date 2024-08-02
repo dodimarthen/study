4900a2b4b5ea85638ce5b4dd0ecef0c62509375b
dodimarthen <dodimartin.sit@gmail.com>
2024-08-02 16:06:30 +0700

    example of simple oop

--- Javascript/part1.js
+++ Javascript/part1.js
@@ -1,14 +1,14 @@
 //Simple Variable
 let myvar = 2;
-console.log(myvar);
-console.log(myvar ** 2);
+// console.log(myvar);
+// console.log(myvar ** 2);
 
 //Data Types and Structures
 let NewVar = 1000;
-console.log(typeof NewVar);
+// console.log(typeof NewVar);
 NewVar = NewVar.toString();
-console.log(NewVar);
-console.log(typeof NewVar);
+// console.log(NewVar);
+// console.log(typeof NewVar);
 
 //Object Oriented Programming
 class Person {
@@ -24,6 +24,36 @@ class Person {
     return this.Age;
   };
 }
-let PersonFirst = new Person("John", 20);
-console.log(PersonFirst.getName());
-console.log(PersonFirst.getAge());
+
+class Home {
+  constructor(address, postalcode, residents) {
+    this.address = address;
+    this.postalcode = postalcode;
+    this.residents = residents;
+  }
+  getAddress = () => {
+    return this.address;
+  };
+
+  getPostalCode = () => {
+    return this.postalcode;
+  };
+
+  getResidents = () => {
+    return this.residents;
+  };
+
+  addResident = (resident) => {
+    this.residents.push(resident);
+  };
+}
+
+let John = new Person("John", 25);
+let Doe = new Person("Doe", 19);
+
+let myhome = new Home("chicago street", 21312, [John, Doe]);
+console.log(myhome.getResidents());
+
+let Janny = new Person("Janny", 28);
+myhome.addResident(Janny);
+console.log(myhome.getResidents());
